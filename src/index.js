e38d34e5ca698d1dcb0975fd82a2c465a88970bf
JessicaAov1 <86833978+JessicaAov1@users.noreply.github.com>
2021-09-11 18:07:26 +0100

    Created a function to get the next days forecast

--- src/index.js
+++ src/index.js
@@ -52,7 +52,8 @@ function formatDate(timestamp) {
   }
   return `Sunset: ${hours}h${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Sun", "Mon", "Tue", "Fri", "Sat"];
 
@@ -83,6 +84,13 @@ function displayForecast() {
 
   forecastElement.innerHTML = forecastHTML;
   console.log(displayForecast);
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9ca6c562062d122440b16668ce916487";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeather(response) {
@@ -118,6 +126,7 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 function showFahrenheit(event) {
@@ -170,5 +179,3 @@ celsiusLink.addEventListener("click", showCelsius);
 
 let cityForm = document.querySelector("#city-form");
 cityForm.addEventListener("submit", handleSubmit);
-
-displayForecast();
